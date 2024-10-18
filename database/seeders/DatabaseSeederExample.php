1597eed9a3ee1c7d8090d539c6f0dbcc75d34c5b
Dan Tang <dan@stats4sd.org>
2024-10-18 10:43:25 +0100

    move team management related seeder files from main repo to submodule; issue: seeder files in submodule cannot be referred in main repo

---
+++ database/seeders/DatabaseSeederExample.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Stats4sd\FilamentTeamManagement\Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Stats4sd\FilamentOdkLink\Database\Seeders\PlatformSeeder;
+
+// This is an example DatabaseSeeder file to be used in main repo.
+// Suppose this file will not be used directly.
+// Please refer to below code and update DatabaseSeeder.php in main repo.
+class DatabaseSeederExample extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(PlatformSeeder::class);
+
+        $this->call([
+            ProgramsTableSeeder::class,
+            TeamsTableSeeder::class,
+            ProgramTeamTableSeeder::class,
+
+            TestUserSeeder::class,
+            ModelHasRolesTableSeeder::class,
+            RoleHasPermissionsTableSeeder::class,
+
+            ProgramUserTableSeeder::class,
+            TeamMembersTableSeeder::class,
+        ]);
+    }
+}
---
+++ database/seeders/ModelHasRolesTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Stats4sd\FilamentTeamManagement\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ModelHasRolesTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        \DB::table('model_has_roles')->delete();
+
+        \DB::table('model_has_roles')->insert(array(
+            0 =>
+            array(
+                'role_id' => 1,
+                'model_type' => 'App\\Models\\User',
+                'model_id' => 2,
+            ),
+        ));
+
+        \DB::table('model_has_roles')->insert(array(
+            0 =>
+            array(
+                'role_id' => 2,
+                'model_type' => 'App\\Models\\User',
+                'model_id' => 3,
+            ),
+        ));
+    }
+}
