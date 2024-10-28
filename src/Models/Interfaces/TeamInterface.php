f75a4d7d7c2617fd9b9ab2efbb23ec5ca1522ebf
dave-mills <dave-mills@users.noreply.github.com>
2024-10-28 11:13:59 +0000

    Fix styling

--- src/Models/Interfaces/TeamInterface.php
+++ src/Models/Interfaces/TeamInterface.php
@@ -8,17 +8,18 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 interface TeamInterface
 {
-
     public function sendInvites(array $emails): void;
 
     public function invites(): HasMany;
-    public function users(): BelongsToMany;
-    public function admins(): BelongsToMany;
-    public function members(): BelongsToMany;
-    public function programs(): BelongsToMany;
 
+    public function users(): BelongsToMany;
+
+    public function admins(): BelongsToMany;
+
+    public function members(): BelongsToMany;
+
+    public function programs(): BelongsToMany;
 
     // add relationship to refer to team model itself, so that app panel > Teams resource can show the selected team for editing
     public function team(): HasOne;
-
 }
