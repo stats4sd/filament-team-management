92c582e3f44f9c52095bef4362719a026164f72f
Dave Mills <d.e.mills@stats4sd.org>
2024-10-28 11:38:38 +0000

    pick any role for inviting; update composer with requirements

--- src/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
+++ src/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
@@ -29,11 +29,8 @@ class ListUsers extends ListRecords
                                 ->email()
                                 ->required(),
 
-                            // Only show "Super Admin" in role selection box.
-                            // This is to avoid admin invite program admin via role-invite.
-                            // Super admin should create a program then invite program admin for that program
                             Forms\Components\Select::make('role')
-                                ->options(Role::where('name', 'Super Admin')->pluck('name', 'id'))
+                                ->relationship('roles', 'name')
                                 ->required(),
                         ])
                         ->reorderable(false)
