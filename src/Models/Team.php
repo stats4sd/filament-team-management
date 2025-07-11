394c5b92dc12c5af7357fc8de17674ab0261dd6e
dan-tang-ssd <86968034+dan-tang-ssd@users.noreply.github.com>
2025-07-11 07:54:46 +0000

    Fix styling

--- src/Models/Team.php
+++ src/Models/Team.php
@@ -47,7 +47,7 @@ class Team extends Model implements TeamInterface
             $user = User::where('email', $email)->first();
 
             // email address does not belong to any registered user
-            if (!$user) {
+            if (! $user) {
                 logger('Email address does not belong to any registered user');
 
                 $invite = $this->invites()->create([
@@ -65,7 +65,7 @@ class Team extends Model implements TeamInterface
                     ->body('An email invitation has been successfully sent to ' . $email)
                     ->send();
 
-            // email address belongs to a registered user
+                // email address belongs to a registered user
             } else {
                 logger('Email address belongs to a registered user');
 
@@ -73,7 +73,7 @@ class Team extends Model implements TeamInterface
                 if ($this->users->contains($user)) {
                     logger('User belongs to this team already');
 
-                    // show notification 
+                    // show notification
                     Notification::make()
                         ->success()
                         ->title('User added')
@@ -83,7 +83,7 @@ class Team extends Model implements TeamInterface
                 } else {
                     logger('User does not belong to this team, add user to this team');
 
-                    // show notification 
+                    // show notification
                     Notification::make()
                         ->success()
                         ->title('User added')
