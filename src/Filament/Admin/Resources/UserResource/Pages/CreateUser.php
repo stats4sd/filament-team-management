18048834deeae6f2ac7c54cc451a102406ae4e22
Dan Tang <dan@stats4sd.org>
2026-05-14 11:10:21 +0100

    crud panel test - fix issue in user create test case

--- src/Filament/Admin/Resources/UserResource/Pages/CreateUser.php
+++ src/Filament/Admin/Resources/UserResource/Pages/CreateUser.php
@@ -11,8 +11,13 @@ class CreateUser extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        // encrypt password before saving it to database
-        $data['password'] = bcrypt('password');
+        // encrypt the user-provided password before saving
+        $data['password'] = bcrypt($data['password']);
+
+        // team_id is a relationship field (Select->relationship('teams', ...)) but its field name
+        // differs from the relationship name, so Filament doesn't strip it from the model data.
+        // Remove it here so it isn't written as a column; saveRelationships() handles the sync.
+        unset($data['team_id']);
 
         return $data;
     }
